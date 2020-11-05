15025adac8e5df13f51e35388aee86cefaba3bf0
CRafa97 <c.ortega@estudiantes.matcom.uh.cu>
2020-11-05 14:40:09 -0500

    working on dev

--- src/fuzzysys.py
+++ src/fuzzysys.py
@@ -1,4 +1,4 @@
-class TriangularFuzzyNumber(object):
+class TriangularFuzzyNumber:
     def __init__(self, inf, pk, sup):
         self.inf = inf
         self.pk = pk
@@ -6,12 +6,12 @@ class TriangularFuzzyNumber(object):
 
     def __call__(self, x):
         "Membership function"
-        return (self.inf <= x <= self.pk) * (x - self.inf) / (self.pk - self.inf) +
+        return (self.inf <= x < self.pk) * (x - self.inf) / (self.pk - self.inf) + \
                (self.pk <= x <= self.sup) * (self.sup - x ) / (self.sup - self.pk)
 
     # define operations
 
-class TrapezoidalFuzzyNumber(object):
+class TrapezoidalFuzzyNumber:
     def __init__(self, inf, pk1, pk2, sup):
         self.inf = inf
         self.pk1 = pk1
@@ -20,6 +20,6 @@ class TrapezoidalFuzzyNumber(object):
 
     def __call__(self, x):
         "Membership function"
-        return (self.inf <= x <= self.pk1) * (x - self.inf) / (self.pk1 - self.inf) +
-               (self.pk1 <= x <= self.pk2) + 
+        return (self.inf <= x < self.pk1) * (x - self.inf) / (self.pk1 - self.inf) + \
+               (self.pk1 <= x < self.pk2) + \
                (self.pk2 <= x <= self.sup) * (self.sup - x) / (self.sup - self.pk2)
